96518260190165fd27ba4ba446c1ff2e8323416b
Your Name <you@example.com>
2024-04-03 13:03:57 +0530

    first commit

---
+++ example.py
@@ -0,0 +1,27 @@
+my_lee="LEESIYA"
+lo_var2=my_lee.lower()
+print(lo_var2)
+
+a="leesiya"
+a=len(a)
+print(a)
+
+var1="happy life"
+var1=len(var1)
+print(var1)
+
+b=[]
+a=b.append("a")
+print(b)
+
+a="welcome to azure family"
+a=len(a)
+print(a)
+
+a="welcome to azure family"
+b=a.upper()
+print(b)
+
+a="welcome to azure family"
+s=a[:10:3]
+print(s)
