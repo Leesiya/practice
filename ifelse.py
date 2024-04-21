8d42ed62aec6bc8f3ff8e9976748ec2203c39eec
leesiya <leesiya.lee.293@gmail.com>
2024-04-21 15:35:17 +0530

    python

---
+++ ifelse.py
@@ -0,0 +1,35 @@
+#if-else function
+
+if(False):
+    print("yes")
+else:
+    print("no")
+
+#comparison operator for Ture and False statements
+
+print("win"=="win")
+
+print("win"=="wins")
+
+#example for if-else and compariosn operator
+
+var1="success"
+if("var1"=="success"):
+    print("Our project is Done")
+else:
+    print("project is unsuccess")
+
+    
+#example two
+
+    
+income=3000
+if(income>7000):
+    print("scolarship is available")
+else:
+    print("not eligible for scolorship")
+
+#remainder
+
+a=11
+print(a%2)
---
+++ variable.py
@@ -0,0 +1,7 @@
+
+Name=input()
+score=int(input())
+Department=input()
+print("my name is:",Name)
+print("my score is:",score/10,"/10")
+print("my department is:",Department)
